a160f49327165d8c9f435e11f94aefa205460fbf
Anna Tuban <mshannahstarling@gmail.com>
2022-01-19 01:34:48 +0300

    feat: destructuring and Conditional Rendering optimized

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-function ImagePopup(props) {
+function ImagePopup({ card, onClose }) {
   return (
-    <div className={`popup page__popup popup_type_image ${props.card && 'popup_opened'}`}>
+    <div className={`popup page__popup popup_type_image ${card && 'popup_opened'}`}>
       <div className='popup__container popup__container_type_image'>
         <figure className='popup__figure'>
-          <img
-            src={props.card ? props.card.link : '#'}
-            className='popup__image'
-            alt={props.card ? props.card.name + '.' : '#'}
-          />
-          <figcaption className='popup__caption'>{props.card.name}</figcaption>
+          <img src={card ? card.link : '#'} className='popup__image' alt={card ? card.name + '.' : '#'} />
+          <figcaption className='popup__caption'>{card.name}</figcaption>
         </figure>
         <button
-          onClick={props.onClose}
+          onClick={onClose}
           className='button popup__btn popup__btn_action_close'
           type='button'
           aria-label='Закрыть картинку.'
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function PopupWithForm(props) {
+function PopupWithForm({ name, isOpen, title, children, onClose }) {
   return (
-    <div className={`popup page__popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`}>
+    <div className={`popup page__popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className='popup__container'>
-        <form className='popup__form' id='card' name={props.name}>
-          <h2 className='popup__title'>{props.title}</h2>
-          {props.children}
+        <form className='popup__form' id='card' name={name}>
+          <h2 className='popup__title'>{title}</h2>
+          {children}
         </form>
         <button
-          onClick={props.onClose}
+          onClick={onClose}
           className='button popup__btn popup__btn_action_close'
           type='button'
           aria-label='Закрыть форму.'
