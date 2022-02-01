b90f182a1a8ef89edbed8e26213be77b0437b636
Anna Tuban <mshannahstarling@gmail.com>
2022-02-01 13:48:21 +0300

    fix: Card props destruct

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function Card(props) {
+function Card({ link, name, onCardClick, likes }) {
   function handleCardClick() {
-    props.onCardClick(props);
+    onCardClick({ name, link });
   }
   return (
     <li className='elements__item'>
-      <img onClick={handleCardClick} src={props.link} className='elements__image' alt={props.name + '.'} />
+      <img onClick={handleCardClick} src={link} className='elements__image' alt={name + '.'} />
       <div className='elements__details-wrapper'>
-        <h2 className='elements__title'>{props.name}</h2>
+        <h2 className='elements__title'>{name}</h2>
         <button className='button elements__like-btn' type='button' aria-label='Поставить лайк.'></button>
-        <span className='elements__like-counter'>{props.likes.length}</span>
+        <span className='elements__like-counter'>{likes.length}</span>
         <button className='button elements__delete-btn' type='button' aria-label='Удалить карточку.'></button>
       </div>
     </li>
