0c3f47aab7eabb2eb1c3eb936353391bb2eb34eb
Anna Tuban <mshannahstarling@gmail.com>
2022-02-10 19:07:51 +0300

    feat: add close action on ESC and overlay

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function Popup({ isForm, btnAriaText, onClose, className, children, ...props }) {
+function Popup({ isOpen, isForm, btnAriaText, onClose, className, children, ...props }) {
+  useEffect(() => {
+    if (!isOpen) return;
+    function closeByEscape(e) {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    }
+    document.addEventListener('keydown', closeByEscape);
+    return () => document.removeEventListener('keydown', closeByEscape);
+  }, [isOpen, onClose]);
+
+  function handleClick() {
+    onClose();
+  }
+
+  function stopHandleClick(e) {
+    e.stopPropagation();
+  }
+
   return (
-    <section className={`popup page__popup ${className}`}>
-      <div className={`popup__container ${isForm ? '' : 'popup__container_type_image'}`.trim()}>
+    <section className={`popup page__popup ${className}`} onClick={handleClick}>
+      <div
+        className={`popup__container ${isForm ? '' : 'popup__container_type_image'}`.trim()}
+        onClick={stopHandleClick}
+      >
         {children}
         <button
           onClick={onClose}
